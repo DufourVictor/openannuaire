3edaa18bc16d30bcc45872c1ca79af67a23d5372
lbourgeois <lucasbourgeois60@hotmail.fr>
2017-12-08 10:07:28 +0100

    Add exemple of integration of material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+}
+
+@Component({
+    selector: 'app-checkboxe',
+    templateUrl: './app.component.html',
+})
+export class CheckboxComponent {
 }
