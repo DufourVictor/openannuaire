027ba8ecaddd4a646c7c6468f784ae0e7eaa8e1e
MaximeBordas <maxime.bordas01@gmail.com>
2017-12-08 11:25:58 +0100

    Revert "Feat/list companies"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+
 import { AppComponent } from './app.component';
-import {HttpModule} from '@angular/http';
-import { ListCompaniesComponent } from './list-companies/list-companies.component';
+
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ListCompaniesComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- src/app/firm-api-interface.ts
+++
@@ -1,7 +0,0 @@
-import {CompanyInterface} from './company-interface';
-
-export interface FirmApiInterface {
-    status: string;
-    params: {};
-    companies: CompanyInterface[];
-}
