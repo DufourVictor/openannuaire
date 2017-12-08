3e3665eeb348cf2a7a114d4554ac1448231118e1
lbourgeois <lucasbourgeois60@hotmail.fr>
2017-12-08 10:17:23 +0100

    last clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
