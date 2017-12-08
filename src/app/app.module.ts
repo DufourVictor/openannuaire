bef8b8e2d40f9a3b49fb6df3014e96e5489434ac
Maxime Bordas <maxime.bordas&@gmail.com>
2017-12-08 11:46:57 +0100

    landing update fix conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-=======
 import {HttpModule} from '@angular/http';
 import { ListCompaniesComponent } from './list-companies/list-companies.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ListCompaniesComponent
+        ListCompaniesComponent,
+        NavbarComponent
     ],
     imports: [
         BrowserModule,
@@ -33,7 +19,6 @@ import { ListCompaniesComponent } from './list-companies/list-companies.componen
     ],
     providers: [],
     bootstrap: [AppComponent]
->>>>>>> 4d67da91cb63ca84b0576ca20fe04ff04733840b
 })
 export class AppModule {
 }
