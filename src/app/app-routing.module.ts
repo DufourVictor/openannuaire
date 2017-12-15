424baffc005efaaeadbe2d534830cf1a78ea857e
Maxime Bordas <maxime.bordas&@gmail.com>
2017-12-15 11:21:46 +0100

    Fix du scss disparu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {ListCompaniesComponent} from './list-companies/list-companies.component';
+import {MapCompaniesComponent} from './map-companies/map-companies.component';
 
 const routes: Routes = [
     {path: '', component: ListCompaniesComponent},
+    {path: 'map', component: MapCompaniesComponent},
 ];
 
 @NgModule({
