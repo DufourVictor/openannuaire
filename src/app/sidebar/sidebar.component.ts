81239b9b53f31cc8d0738ea665dd5cc82e6900eb
Victor <DufourVictor3@gmail.com>
2018-01-17 00:03:18 +0100

    End of show/hide sidebar filters

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -12,6 +12,14 @@ export class SidebarComponent {
 
     // State of filters
     hideApe = true;
+    hideCategories = true;
+    hideCounty = true;
+    hideCity = true;
+    hideCreation = true;
+    hideLegal = true;
+    hideEffectives = true;
+    hideRevenues = true;
+    hidePostal = true;
 
     constructor(private retrieveCompaniesService: RetrieveCompaniesService) {
         this.retrieveCompaniesService.totalCompanies.subscribe(
@@ -25,6 +33,30 @@ export class SidebarComponent {
         switch (filter) {
             case Filters.APE:
                 this.hideApe = visible;
+                break;
+            case Filters.CATEGORIES:
+                this.hideCategories = visible;
+                break;
+            case Filters.COUNTY:
+                this.hideCounty = visible;
+                break;
+            case Filters.CITY:
+                this.hideCity = visible;
+                break;
+            case Filters.CREATION:
+                this.hideCreation = visible;
+                break;
+            case Filters.LEGAL:
+                this.hideLegal = visible;
+                break;
+            case Filters.EFFECTIVES:
+                this.hideEffectives = visible;
+                break;
+            case Filters.REVENUES:
+                this.hideRevenues = visible;
+                break;
+            case Filters.POSTAL:
+                this.hidePostal = visible;
                 break;
         }
     }
