157f8f2d461916fead8f6021e92105d4f52d2e46
Victor <DufourVictor3@gmail.com>
2018-01-17 23:31:29 +0100

    Update county filter

--- src/app/Model/default-filters.ts
+++ src/app/Model/default-filters.ts
@@ -24,112 +24,110 @@ export class DefaultFilters {
         'GE',
     ];
 
-    static postals() {
-        let postalsArray = new Array();
-        postalsArray['01'] = 'Ain';
-        postalsArray['02'] = 'Aisne';
-        postalsArray['03'] = 'Allier';
-        postalsArray['04'] = 'Alpes de Haute Provence';
-        postalsArray['05'] = 'Hautes Alpes';
-        postalsArray['06'] = 'Alpes Maritimes';
-        postalsArray['07'] = 'Ardèche';
-        postalsArray['08'] = 'Ardennes';
-        postalsArray['09'] = 'Ariège';
-        postalsArray['10'] = 'Aube';
-        postalsArray['11'] = 'Aude';
-        postalsArray['12'] = 'Aveyron';
-        postalsArray['13'] = 'Bouches du Rhône';
-        postalsArray['14'] = 'Calvados';
-        postalsArray['15'] = 'Cantal';
-        postalsArray['16'] = 'Charente';
-        postalsArray['17'] = 'Charente Maritime';
-        postalsArray['18'] = 'Cher';
-        postalsArray['19'] = 'Corrèze';
-        postalsArray['200'] = 'Corse du Sud';
-        postalsArray['201'] = 'Haute-Corse';
-        postalsArray['21'] = 'Côte d\'Or';
-        postalsArray['22'] = 'Côtes d\'Armor';
-        postalsArray['23'] = 'Creuse';
-        postalsArray['24'] = 'Dordogne';
-        postalsArray['25'] = 'Doubs';
-        postalsArray['26'] = 'Drôme';
-        postalsArray['27'] = 'Eure';
-        postalsArray['28'] = 'Eure et Loir';
-        postalsArray['29'] = 'Finistère';
-        postalsArray['30'] = 'Gard';
-        postalsArray['31'] = 'Haute Garonne';
-        postalsArray['32'] = 'Gers';
-        postalsArray['33'] = 'Gironde';
-        postalsArray['34'] = 'Hérault';
-        postalsArray['35'] = 'Ille et Vilaine';
-        postalsArray['36'] = 'Indre';
-        postalsArray['37'] = 'Indre et Loire';
-        postalsArray['38'] = 'Isère';
-        postalsArray['39'] = 'Jura';
-        postalsArray['40'] = 'Landes';
-        postalsArray['41'] = 'Loir et Cher';
-        postalsArray['42'] = 'Loire';
-        postalsArray['43'] = 'Haute Loire';
-        postalsArray['44'] = 'Loire Atlantique';
-        postalsArray['45'] = 'Loiret';
-        postalsArray['46'] = 'Lot';
-        postalsArray['47'] = 'Lot et Garonne';
-        postalsArray['48'] = 'Lozère';
-        postalsArray['49'] = 'Maine et Loire';
-        postalsArray['50'] = 'Manche';
-        postalsArray['51'] = 'Marne';
-        postalsArray['52'] = 'Haute Marne';
-        postalsArray['53'] = 'Mayenne';
-        postalsArray['54'] = 'Meurthe et Moselle';
-        postalsArray['55'] = 'Meuse';
-        postalsArray['56'] = 'Morbihan';
-        postalsArray['57'] = 'Moselle';
-        postalsArray['58'] = 'Nièvre';
-        postalsArray['59'] = 'Nord';
-        postalsArray['60'] = 'Oise';
-        postalsArray['61'] = 'Orne';
-        postalsArray['62'] = 'Pas de Calais';
-        postalsArray['63'] = 'Puy de Dôme';
-        postalsArray['64'] = 'Pyrénées Atlantiques';
-        postalsArray['65'] = 'Hautes Pyrénées';
-        postalsArray['66'] = 'Pyrénées Orientales';
-        postalsArray['67'] = 'Bas Rhin';
-        postalsArray['68'] = 'Haut Rhin';
-        postalsArray['69'] = 'Rhône';
-        postalsArray['70'] = 'Haute Saône';
-        postalsArray['71'] = 'Saône et Loire';
-        postalsArray['72'] = 'Sarthe';
-        postalsArray['73'] = 'Savoie';
-        postalsArray['74'] = 'Haute Savoie';
-        postalsArray['75'] = 'Paris';
-        postalsArray['76'] = 'Seine Maritime';
-        postalsArray['77'] = 'Seine et Marne';
-        postalsArray['78'] = 'Yvelines';
-        postalsArray['79'] = 'Deux Sèvres';
-        postalsArray['80'] = 'Somme';
-        postalsArray['81'] = 'Tarn';
-        postalsArray['82'] = 'Tarn et Garonne';
-        postalsArray['83'] = 'Var';
-        postalsArray['84'] = 'Vaucluse';
-        postalsArray['85'] = 'Vendée';
-        postalsArray['86'] = 'Vienne';
-        postalsArray['87'] = 'Haute Vienne';
-        postalsArray['88'] = 'Vosges';
-        postalsArray['89'] = 'Yonne';
-        postalsArray['90'] = 'Territoire de Belfort';
-        postalsArray['91'] = 'Essonne';
-        postalsArray['92'] = 'Hauts de Seine';
-        postalsArray['93'] = 'Seine Saint Denis';
-        postalsArray['94'] = 'Val de Marne';
-        postalsArray['95'] = 'Val d\'Oise';
-        postalsArray['971'] = 'Guadeloupe';
-        postalsArray['972'] = 'Martinique';
-        postalsArray['973'] = 'Guyane';
-        postalsArray['974'] = 'Réunion';
-        postalsArray['975'] = 'Saint Pierre et Miquelon';
-        postalsArray['976'] = 'Mayotte';
-    }
-
+    static county = [
+        {'name': 'Ain', 'value': '01'},
+        {'name': 'Aisne', 'value': '02'},
+        {'name': 'Allier', 'value': '03'},
+        {'name': 'Alpes de Haute Provence', 'value': '04'},
+        {'name': 'Hautes Alpes', 'value': '05'},
+        {'name': 'Alpes Maritimes', 'value': '06'},
+        {'name': 'Ardèche', 'value': '07'},
+        {'name': 'Ardennes', 'value': '08'},
+        {'name': 'Ariège', 'value': '09'},
+        {'name': 'Aube', 'value': '10'},
+        {'name': 'Aude', 'value': '11'},
+        {'name': 'Aveyron', 'value': '12'},
+        {'name': 'Bouches du Rhône', 'value': '13'},
+        {'name': 'Calvados', 'value': '14'},
+        {'name': 'Cantal', 'value': '15'},
+        {'name': 'Charente', 'value': '16'},
+        {'name': 'Charente Maritime', 'value': '17'},
+        {'name': 'Cher', 'value': '18'},
+        {'name': 'Corrèze', 'value': '19'},
+        {'name': 'Corse du Sud', 'value': '2A'},
+        {'name': 'Haute-Corse', 'value': '2B'},
+        {'name': 'Côte d\'Or', 'value': '21'},
+        {'name': 'Côtes d\'Armor', 'value': '22'},
+        {'name': 'Creuse', 'value': '23'},
+        {'name': 'Dordogne', 'value': '24'},
+        {'name': 'Doubs', 'value': '25'},
+        {'name': 'Drôme', 'value': '26'},
+        {'name': 'Eure', 'value': '27'},
+        {'name': 'Eure et Loir', 'value': '28'},
+        {'name': 'Finistère', 'value': '29'},
+        {'name': 'Gard', 'value': '30'},
+        {'name': 'Haute Garonne', 'value': '31'},
+        {'name': 'Gers', 'value': '32'},
+        {'name': 'Gironde', 'value': '33'},
+        {'name': 'Hérault', 'value': '34'},
+        {'name': 'Ille et Vilaine', 'value': '35'},
+        {'name': 'Indre', 'value': '36'},
+        {'name': 'Indre et Loire', 'value': '37'},
+        {'name': 'Isère', 'value': '38'},
+        {'name': 'Jura', 'value': '39'},
+        {'name': 'Landes', 'value': '40'},
+        {'name': 'Loir et Cher', 'value': '41'},
+        {'name': 'Loire', 'value': '42'},
+        {'name': 'Haute Loire', 'value': '43'},
+        {'name': 'Loire Atlantique', 'value': '44'},
+        {'name': 'Loiret', 'value': '45'},
+        {'name': 'Lot', 'value': '46'},
+        {'name': 'Lot et Garonne', 'value': '47'},
+        {'name': 'Lozère', 'value': '48'},
+        {'name': 'Maine et Loire', 'value': '49'},
+        {'name': 'Manche', 'value': '50'},
+        {'name': 'Marne', 'value': '51'},
+        {'name': 'Haute Marne', 'value': '52'},
+        {'name': 'Mayenne', 'value': '53'},
+        {'name': 'Meurthe et Moselle', 'value': '54'},
+        {'name': 'Meuse', 'value': '55'},
+        {'name': 'Morbihan', 'value': '56'},
+        {'name': 'Moselle', 'value': '57'},
+        {'name': 'Nièvre', 'value': '58'},
+        {'name': 'Nord', 'value': '59'},
+        {'name': 'Oise', 'value': '60'},
+        {'name': 'Orne', 'value': '61'},
+        {'name': 'Pas de Calais', 'value': '62'},
+        {'name': 'Puy de Dôme', 'value': '63'},
+        {'name': 'Pyrénées Atlantiques', 'value': '64'},
+        {'name': 'Hautes Pyrénées', 'value': '65'},
+        {'name': 'Pyrénées Orientales', 'value': '66'},
+        {'name': 'Bas Rhin', 'value': '67'},
+        {'name': 'Haut Rhin', 'value': '68'},
+        {'name': 'Rhône', 'value': '69'},
+        {'name': 'Haute Saône', 'value': '70'},
+        {'name': 'Saône et Loire', 'value': '71'},
+        {'name': 'Sarthe', 'value': '72'},
+        {'name': 'Savoie', 'value': '73'},
+        {'name': 'Haute Savoie', 'value': '74'},
+        {'name': 'Paris', 'value': '75'},
+        {'name': 'Seine Maritime', 'value': '76'},
+        {'name': 'Seine et Marne', 'value': '77'},
+        {'name': 'Yvelines', 'value': '78'},
+        {'name': 'Deux Sèvres', 'value': '79'},
+        {'name': 'Somme', 'value': '80'},
+        {'name': 'Tarn', 'value': '81'},
+        {'name': 'Tarn et Garonne', 'value': '82'},
+        {'name': 'Var', 'value': '83'},
+        {'name': 'Vaucluse', 'value': '84'},
+        {'name': 'Vendée', 'value': '85'},
+        {'name': 'Vienne', 'value': '86'},
+        {'name': 'Haute Vienne', 'value': '87'},
+        {'name': 'Vosges', 'value': '88'},
+        {'name': 'Yonne', 'value': '89'},
+        {'name': 'Territoire de Belfort', 'value': '90'},
+        {'name': 'Essonne', 'value': '91'},
+        {'name': 'Hauts de Seine', 'value': '92'},
+        {'name': 'Seine Saint Denis', 'value': '93'},
+        {'name': 'Val de Marne', 'value': '94'},
+        {'name': 'Val d\'Oise', 'value': '95'},
+        {'name': 'Guadeloupe', 'value': '9A'},
+        {'name': 'Martinique', 'value': '9B'},
+        {'name': 'Guyane', 'value': '9C'},
+        {'name': 'Réunion', 'value': '9D'},
+        {'name': 'Saint Pierre et Miquelon', 'value': ''},
+        {'name': 'Mayotte', 'value': '9F'},
+    ];
 
     static turnover = [
         'Moins de 0,5 millions d\'euros',
